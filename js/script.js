a6866b058922239b48a86c43609f6060ff1a4b63
jeironpro <jeironprogrammer@gmail.com>
2026-08-05 19:17:59 +0200

    Agregando descripcion faltante

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const datosMarcadores = {
         { nombre: "Threads", descripcion: "Plataforma de Meta para compartir actualizaciones de texto y unirse a conversaciones públicas.", url: "https://www.threads.com/" },
         { nombre: "Instagram", descripcion: "Red social para compartir fotos y videos.", url: "https://www.instagram.com/" },
         { nombre: "Facebook", descripcion: "Red social para conectar con amistades, familiares y comunidades de personas que comparten tus intereses.", url: "https://www.facebook.com/" },
-        { nombre: "TikTok", descripcion: "", url: "https://www.tiktok.com/" },
+        { nombre: "TikTok", descripcion: "TikTok es una plataforma global para descubrir vídeos, transmitir en LIVE, comprar y más. Nuestra misión consiste en potenciar la creatividad y hacer disfrutar a la gente. Más de mil millones de personas vienen a TikTok para compartir lo que les gusta, aprender cosas nuevas y encontrar ideas para que el día a día sea más interesante.", url: "https://www.tiktok.com/" },
     ],
     "Gestión": [
         { nombre: "GitHub", descripcion: "Plataforma principal para alojamiento de código e iteración de desarrollo.", url: "https://github.com/" },
